f2e15413f8f1b04d375afefd4be5fb265b59ba57
izgarshev <izgarshev_p@mail.ru>
2021-01-27 16:51:24 +0300

    ex7-8

---
+++ src/main/java/ex7/Main.java
@@ -0,0 +1,114 @@
+package main.java.ex7;
+
+import org.w3c.dom.ls.LSOutput;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * 1. Implement the “Sieve of Erathostenes” algorithm to determine all prime numbers
+ * ≤ n. Add all numbers from 2 to n to a set. Then repeatedly find the smallest
+ * element s in the set, and remove s
+ * 2
+ * , s · (s + 1), s · (s + 2), and so on. You are done
+ * when s
+ * 2
+ * > n. Do this with both a HashSet<Integer> and a BitSet.
+ * 2. In an array list of strings, make each string uppercase. Do this with (a) an iterator,
+ * (b) a loop over the index values, and (c) the replaceAll method.
+ * 3. How do you compute the union, intersection, and difference of two sets, using just
+ * the methods of the Set interface and without using loops?
+ * 4. Produce a situation that yields a ConcurrentModificationException.
+ * What can you do to avoid it?
+ * <p>
+ * 5. Implement a method public static void swap(List<?> list,
+ * int i, int j) that swaps elements in the usual way when the type of list
+ * implements the RandomAccess interface, and that minimizes the cost of visiting
+ * the positions at index i and j if it is not.
+ * 6. I encouraged you to use interfaces instead of concrete data structures—for
+ * example, a Map instead of a TreeMap. Unfortunately, that advice goes only so
+ * far. Suppose you have a method parameter of type Map<String,
+ * Set<Integer>>, and someone calls your method with a HashMap<String,
+ * HashSet<Integer>>. What happens? What parameter type can you use
+ * instead?
+ * 7. Write a program that reads all words in a file and prints out how often each word
+ * occurred. Use a TreeMap<String, Integer>.
+ */
+public class Main {
+    private static File textFile = new File("src/main/java/ex7/text.txt");
+    public static void main(String[] args) throws FileNotFoundException {
+        solution4();
+        swap(new ArrayList<>(Arrays.asList(1,2,3,4,5)), 2, 3);//solution5
+        solution6(new HashMap<String, HashSet<Integer>>());
+        solution7();
+    }
+
+    /**
+     * самый простой вариант - заменить параметр Set на HashSet
+     * @param map
+     */
+    private static void solution6(HashMap<String, HashSet<Integer>> map) {
+        System.out.println(map);
+    }
+
+
+
+    private static void solution4() {
+        //здесь будет ConcurrentModificationException, т.к мы пытаемся внести изменения в коллекцию во время работы итератора
+//        List<Integer> list = new ArrayList<>(Arrays.asList(0, 1, 2, 3, 4));
+//        for (Integer number : list) {
+//            System.out.println("number: " + number);
+//            if (number.equals(2)) {
+//                list.add(5);
+//            }
+//        }
+        //чтобы избежать исключения мы добавляем новый элемент после работы итератора
+        List<Integer> list = new ArrayList<>(Arrays.asList(0, 1, 2, 3, 4));
+        for (Integer number : list) {
+            System.out.println("number: " + number);
+        }
+            list.add(5);
+        for (Integer number : list) {
+            System.out.println("number: " + number);
+        }
+    }
+
+    /**
+     * метод меняет местами элементы, под указанными номерами
+     */
+    public static void swap(List<?> list, int i, int j) {
+        System.out.println(list);
+        final List l = list;
+        l.set(i, l.set(j, l.get(i)));
+        System.out.println(list);
+    }
+
+    private static void solution7() throws FileNotFoundException {
+        List<String> words = loadText(textFile);
+        System.out.println("Occurrences: " + countOccurrence(words).toString());
+    }
+
+    private static Map<String, Integer> countOccurrence(List<String> words) {
+        Map<String, Integer> occurrences = new TreeMap<>();
+        for (String word : words) {
+            occurrences.put(word, occurrences.getOrDefault(word, 0) + 1);
+        }
+        return occurrences;
+    }
+
+    private static List<String> loadText(File filename) throws FileNotFoundException {
+        if (filename == null || !filename.exists()) {
+            throw new IllegalArgumentException("Error: filename incorrect");
+        }
+
+        List<String> result = new ArrayList<>();
+        Scanner in = new Scanner(filename);
+        while (in.hasNext()) {
+            result.add(in.next());
+        }
+
+        return result;
+    }
+}
---
+++ src/main/java/ex8/Main.java
@@ -0,0 +1,77 @@
+package main.java.ex8;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+/**
+ * 1. Verify that asking for the first five long words does not call the filter method
+ * once the fifth long word has been found. Simply log each method call.
+ * 2. Measure the difference when counting long words with a parallelStream
+ * instead of a stream. Call System.currentTimeMillis before and after
+ * the call and print the difference. Switch to a larger document (such as War and
+ * Peace) if you have a fast computer.
+ * 3. Suppose you have an array int[] values = { 1, 4, 9, 16 }. What is
+ * <p>
+ * Stream.of(values)? How do you get a stream of int instead?
+ * 4. Using Stream.iterate, make an infinite stream of random numbers—not by
+ * calling Math.random but by directly implementing a linear congruential
+ * generator. In such a generator, you start with x0
+ * = seed and then produce xn + 1 =
+ * (a xn
+ * + c) % m, for appropriate values of a, c, and m. You should implement a
+ * method with parameters a, c, m, and seed that yields a Stream<Long>. Try
+ * out a = 25214903917, c = 11, and m = 248
+ * .
+ * 5. The codePoints method in Section 8.3, “The filter, map, and flatMap
+ * Methods” (page 263) was a bit clumsy, first filling an array list and then turning it
+ * into a stream. Write a stream-based version instead, using the
+ * IntStream.iterate method to construct a finite stream of offsets, then
+ * extract the substrings.
+ * 6. Use the String.codePoints method to implement a method that tests
+ * whether a string is a word, consisting only of letters. (Hint:
+ * Character.isAlphabetic.) Using the same approach, implement a method
+ * that tests whether a string is a valid Java identifier.
+ * 7. Turning a file into a stream of tokens, list the first 100 tokens that are words in the
+ * sense of the preceding exercise. Read the file again and list the 10 most frequent
+ * words, ignoring letter case.
+ */
+public class Main {
+    public static void main(String[] args) {
+//        System.out.println(solution5("solution"));
+        System.out.println(solution6("some string"));
+    }
+
+    /**
+     * возвращает IntStream с шагом 1 и величиной в длину переданной строки
+     */
+    public static IntStream solution5(String s) {
+        return IntStream.iterate(0, value -> value < s.length(), operand -> operand + 1);
+    }
+
+    /**
+     * проверяет является ли строка словом, состоящим из букв
+     */
+    private static boolean solution6(String s) {
+        codePoints(s).forEach(s1 -> System.out.println(s1));
+        return true;
+    }
+
+    private static void solution7() {
+
+    }
+
+    public static Stream<String> codePoints(String s) {
+        List<String> result = new ArrayList<>();
+        int i = 0;
+        while (i < s.length()) {
+            int j = s.offsetByCodePoints(i, 1);
+            result.add(s.substring(i, j));
+            i = j;
+        }
+        return result.stream();
+    }
+
+
+}
